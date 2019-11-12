bfb7c85a134e756294156ad0f5ee567f860f61d8
TheMindset <mulezig2014@outlook.fr>
2019-11-12 14:16:10 +0100

    test dark_sky services

---
+++ spec/services/dark_sky_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe DarkSky do
+  before do
+    stub_json("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/48.856614,2.3522219?lang=fr&units=si",
+    "./fixtures/paris_forecast.json")
+    end
+
+    let!(:dark_sky) { described_class.new }
+
+    it "exists" do
+      expect(dark_sky).to be_a(described_class)
+    end
+
+    it "retreive forecast for a specific location" do
+      forecast = dark_sky.retreive_forecast("48.856614,2.3522219")
+
+      expect(forecast).to be_a(Hash)
+      expect(forecast[:latitude]).to eq(48.856614)
+      expect(forecast[:longitude]).to eq(2.3522219)
+    end
+  
+end
