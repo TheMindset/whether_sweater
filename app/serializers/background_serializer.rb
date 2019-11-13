5fe3a9fec7f168e5c3b6c43b5bcced99be2e9bdf
TheMindset <mulezig2014@outlook.fr>
2019-11-13 14:19:33 +0100

    moer design for background serializer

--- app/serializers/background_serializer.rb
+++ app/serializers/background_serializer.rb
@@ -11,31 +11,31 @@ class BackgroundSerializer
           },
           {
             alt: background_images[1][:title],
-            url: "https://www.flickr.com/photos/#{background_images[1][:owner]}/#{background_images[1][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[1][:owner]}/#{background_images[1][:id]}"
           },
           {
             alt: background_images[2][:title],
-            url: "https://www.flickr.com/photos/#{background_images[2][:owner]}/#{background_images[2][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[2][:owner]}/#{background_images[2][:id]}"
           },
           {
             alt: background_images[3][:title],
-            url: "https://www.flickr.com/photos/#{background_images[3][:owner]}/#{background_images[3][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[3][:owner]}/#{background_images[3][:id]}"
           },
           {
             alt: background_images[4][:title],
-            url: "https://www.flickr.com/photos/#{background_images[4][:owner]}/#{background_images[4][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[4][:owner]}/#{background_images[4][:id]}"
           },
           {
             alt: background_images[5][:title],
-            url: "https://www.flickr.com/photos/#{background_images[5][:owner]}/#{background_images[5][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[5][:owner]}/#{background_images[5][:id]}"
           },
           {
             alt: background_images[6][:title],
-            url: "https://www.flickr.com/photos/#{background_images[6][:owner]}/#{background_images[6][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[6][:owner]}/#{background_images[6][:id]}"
           },
           {
             alt: background_images[7][:title],
-            url: "https://www.flickr.com/photos/#{background_images[7][:owner]}/#{background_images[7][:id]}",
+            url: "https://www.flickr.com/photos/#{background_images[7][:owner]}/#{background_images[7][:id]}"
           }
         ]
       }
