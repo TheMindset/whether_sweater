ec6e5b5e91847e7665ea1cb6a185be32e257cdfe
TheMindset <mulezig2014@outlook.fr>
2019-11-13 14:53:27 +0100

    account creation without valid params test

--- spec/request/user/account_creation_spec.rb
+++ spec/request/user/account_creation_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'User API endpoint', type: :feature do
-  it "#create accountwith a valid params" do
+  it "#create account with a valid params" do
     params = {
-      email: 'test@yopmail.com'
-      password: 'password'
+      email: 'test@yopmail.com',
+      password: 'password',
       password_confirmation: 'password'
     }
 
@@ -15,5 +17,19 @@ RSpec.describe 'User API endpoint', type: :feature do
     response_body = JSON.parse(repsonse.body, symbolize_names: true)
 
     expect(response_body).to have_key(:api_key)
+  end
+
+  it "#creation account without a valid params" do
+    params = {
+      email: 'test@yopmail.com'
+    }
+
+    post '/api/v1/users', params: params
+
+    expect(repsonse.status).to eq(400)
+
+    response_body = JSON.parse(repsonse.body, symbolize_names: true)
+
+    expect(response_body[:error]).to eq('Failed to create user')
   end
 end
