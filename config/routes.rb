8f391374967ebddf0c51f0a3302ddd3f3665032d
TheMindset <mulezig2014@outlook.fr>
2019-11-13 14:15:51 +0100

    redefine route for background

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#show'
-      get 'backgrounds', to: 'backgrounds#show'
+      get 'background', to: 'backgrounds#show'
     end
   end
 end
