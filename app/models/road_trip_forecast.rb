2abb77b14751da325b5b1a3f539ba63d997d27af
TheMindset <mulezig2014@outlook.fr>
2019-11-15 20:14:09 +0100

    design road trip response

--- app/models/road_trip_forecast.rb
+++ app/models/road_trip_forecast.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RoadTripForecast
-  attr_reader :origin, :destination
+  attr_reader :origin, :destination, :total_duration
 
   def initialize(drive, forecast)
     @origin = drive[:start_address]
@@ -9,5 +9,20 @@ class RoadTripForecast
     @total_distance = drive[:distance][:text]
     @total_duration = drive[:duration][:text]
     @forecast = forecast
+    @duration_drive = drive[:duration][:value]
+  end
+
+  def arrival_weather
+    @arrival_weather ||= forecast.hourly.min do |forecast|
+      (arrival_time - forecast[:time]).abs
+    end
+  end
+
+  private
+
+  attr_reader :duration_drive, :forecast
+
+  def arrival_time
+    duration_drive + Time.now.to_i
   end
 end
--- app/serializers/road_trip_forecast_serializer.rb
+++ app/serializers/road_trip_forecast_serializer.rb
@@ -5,7 +5,15 @@ class RoadTripForecastSerializer
     {
       data: {
         origin: retreive_road_trip.origin,
-        destination: retreive_road_trip.destination
+        destination: retreive_road_trip.destination,
+        duration_course: retreive_road_trip.total_duration,
+        wheather_on_arrival: {
+          summary: retreive_road_trip.arrival_weather[:summary],
+          temperature: retreive_road_trip.arrival_weather[:temperature],
+          feels_like: retreive_road_trip.arrival_weather[:apparentTemperature],
+          visibility_miles: retreive_road_trip.arrival_weather[:visibility],
+          uv_index: retreive_road_trip.arrival_weather[:uvIndex]
+        }
       }
     }
   end
