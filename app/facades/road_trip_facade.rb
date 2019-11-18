47a6bcc38c7af8a84964d07e0760f20e137d278b
TheMindset <mulezig2014@outlook.fr>
2019-11-18 18:02:59 +0100

    caching road_trip informations from model

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -6,10 +6,14 @@ class RoadTripFacade
   end
 
   def retreive_road_trip
-    @retreive_road_trip ||= RoadTripForecast.new(drive, forecast)
+    Rails.cache.fetch("forecast-for-#{road_trip_params[:origin]}-#{road_trip_params[:destination]}", expires_in: 2.minutes) do
+      RoadTripForecast.new(drive, forecast)
+    end
   end
 
   private
+
+  attr_reader :road_trip_params
 
   def drive
     @drive ||= direction.retreive_road_trip
