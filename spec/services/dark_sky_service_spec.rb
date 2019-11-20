f205af80c368db596ce560e4c41846e459245629
TheMindset <mulezig2014@outlook.fr>
2019-11-20 16:04:23 +0100

    refactor spec darksky_serivice

--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -2,18 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe DarkSkyService, :vcr do
-  let!(:dark_sky) { described_class.new }
-
-  it "exists" do
-    expect(dark_sky).to be_a(described_class)
-  end
-
+RSpec.describe DarkSkyService do
   it "retreive forecast for a specific location" do
-    forecast = dark_sky.retreive_forecast("48.856614,2.3522219")
+    dark_sky = described_class.new
 
-    expect(forecast).to be_a(Hash)
-    expect(forecast[:latitude]).to eq(48.856614)
-    expect(forecast[:longitude]).to eq(2.3522219)
+    VCR.use_cassette('retreive_forecast_response') do
+      response = dark_sky.retreive_forecast("48.856614,2.3522219")
+
+      expect(response).to be_a(Hash)
+      expect(response[:latitude]).to eq(48.856614)
+      expect(response[:longitude]).to eq(2.3522219)
+    end
   end
 end
