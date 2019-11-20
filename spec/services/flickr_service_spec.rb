5380e7ab0a263bddb59f00888a5e091db56005a8
TheMindset <mulezig2014@outlook.fr>
2019-11-20 16:03:19 +0100

    refactor spec flickr_service

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -2,18 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe FlickrService, :vcr do
-  let!(:pictures) { described_class.new(48.856614, 2.3522219, 'downtown,sky') }
-
-  it "exists" do
-    expect(pictures).to be_a(described_class)
-  end
-
+RSpec.describe FlickrService do
   it "retreive images for a specific location" do
-    first_picture = pictures.image_search
+    pictures = described_class.new(48.856614, 2.3522219, 'downtown,sky')
 
-    expect(first_picture).to be_a(Hash)
-    expect(first_picture[:photos][:photo][0][:id]).to eq("45076145644")
-    expect(first_picture[:photos][:photo][0][:title]).to eq("Paris sera toujours Paris")
+    VCR.use_cassette('retreive_images_response') do
+      first_picture = pictures.image_search
+
+      expect(first_picture).to be_a(Hash)
+      expect(first_picture[:photos][:photo][0][:id]).to eq("45076145644")
+      expect(first_picture[:photos][:photo][0][:title]).to eq("Paris sera toujours Paris")
+    end
   end
 end
