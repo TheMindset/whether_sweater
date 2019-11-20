6de5e919097437e391d1c0ce1c08cb19136a8b75
TheMindset <mulezig2014@outlook.fr>
2019-11-20 16:02:48 +0100

    refactor  spec geocoder_service

--- spec/services/geocoder_service_spec.rb
+++ spec/services/geocoder_service_spec.rb
@@ -2,17 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe GeocoderService, :vcr do
-  let!(:geocoder) { described_class.new('Paris') }
+RSpec.describe GeocoderService do
+  it '#retreive lat and long for a specific location' do
+    geocoder = described_class.new('Paris')
 
-  it "exists" do
-    expect(geocoder).to be_a(described_class)
-  end
+    VCR.use_cassette('geocoder_service_response') do
+      location = geocoder.coords
 
-  it '#retreive lat and lond for a specific location' do
-    location = geocoder.coords
-    expect(location).to be_a(Hash)
-    expect(location[:results][0][:geometry][:location][:lat]).to eq(48.856614)
-    expect(location[:results][0][:geometry][:location][:lng]).to eq(2.3522219)
+      expect(location).to be_a(Hash)
+      expect(location[:results][0][:geometry][:location][:lat]).to eq(48.856614)
+      expect(location[:results][0][:geometry][:location][:lng]).to eq(2.3522219)
+    end
   end
 end
